37a3ca08795af3a2af39130c79d4351a2ac6dda5
Johan Jatko <armedguy@ludd.ltu.se>
2018-11-20 21:57:42 +0100

    add docker to vagrant

--- vagrant_provision.sh
+++ vagrant_provision.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+apt-get update
+apt-get install -y \
+    debootstrap \
+    docker.io \
+    jq \
+    kpartx \
+    parted \
+    python-docopt \
+    python-functools32 \
+    python-fysom \
+    python-jsonschema \
+    python-requests \
+    python-setuptools \
+    python-termcolor \
+    python-yaml \
+    qemu-utils
+
+adduser vagrant docker
