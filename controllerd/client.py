e6f7ceca5e837c1770b4a2aa7a6bd80ea7f39011
Johan Jatko <armedguy@ludd.ltu.se>
2018-12-16 12:44:04 +0100

    new client and server

--- controllerd/client.py
+++ controllerd/client.py
@@ -4,21 +4,49 @@ import asyncio
 import websockets
 import json
 
+ws = None
+
+async def send_stuff():
+    while True:
+        await websocket.send(json.dumps({
+            "rpc": "stats",
+            "stats": {
+                "cpu": 30,
+                "ram": 200
+            }
+        }))
+        await websocket.send(json.dumps({
+            "rpc": "output",
+            "output": {
+                "stdout": "KASDJASDJASD",
+                "stderr": ""
+            }
+        }))
+        asyncio.sleep(30)
+
 async def controller(uri):
     async with websockets.connect(uri) as websocket:
         print("connected, sending!")
+        ws = websocket
         await websocket.send(json.dumps({
             "rpc": "register",
             "data": {
-                "id": "bla",
-                "model": "bla",
-                "cpus": 2,
-                "ram": 2048
+                "id": None,
+                "model": "Zynq 700 Swag",
+                "cpu": 2,
+                "ram": 2048,
+                "status": "online"
             }
         }))
         print("sent, awaiting response!")
         register_response = await websocket.recv()
         print("this is response", register_response)
+        while True:
+            msg = json.loads(await websocket.recv())
+            print(msg)
 
 asyncio.get_event_loop().run_until_complete(
-    controller('ws://localhost:8765'))
+    asyncio.gather(
+        controller('ws://localhost:8765'),
+        send_stuff()
+    )
--- controllerd/server.py
+++ controllerd/server.py
@@ -5,45 +5,66 @@ import websockets
 import json
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient("mongodb://130.240.200.99:27017/")
 db = client.iot_platform
 stats_c = db.stats
 devices_c = db.devices
 output_c = db.output
+files_c = db.files
 
-def register(websocket, data):
+async def register(websocket, data):
     if data["id"] is None:
         data["id"] = devices_c.insert_one(data).inserted_id
     else:
         devices_c.update({"_id": data["id"]}, data)
-    websocket._device_id = data["id"]
+    websocket._device_id = str(data["id"])
     print("registered device {}".format(data["id"]))
     await websocket.send(json.dumps({"status": "OK", "device_id": websocket._device_id}))
 
-def stats(websocket, stats):
+async def stats(websocket, stats):
     stats["device_id"] = websocket._device_id
     stats_c.update({"device_id": websocket._device_id}, stats, upsert=True)
 
-def output(websocket, output):
+async def output(websocket, output):
     output["device_id"] = websocket._device_id
     output_c.update({"device_id": websocket._device_id}, output, upsert=True)
 
+async def status(websocket, status):
+    devices_c.update({"_id": websocket._device_id}, {"status": status})
 
+async def do_stuff_timer():
+    while True:
+        queued = devices_c.find({"status": "flash_queued"})
+        for device in queued:
+            devices_c.update({"_id": device["_id"]}, {"status": "flashing"})
+            await wslist[device['_id']].send({"rpc": "flash", "url": "http://130.240.200.99/api/file/" + str(device["_id"]) + ".tar.gz"})
+        await asyncio.sleep(10)
+    pass
+
+wslist = {}
 async def handle(websocket, path):
     async for message in websocket:
         print(message)
         decoded = json.loads(message)
-        if decoded["rpc"] == "ping":
-            websocket.send(json.dumps({"rpc": "pong"}))
-        elif decoded["rpc"] == "stats":
-            stats(websocket, decoded["stats"])
+        if decoded["rpc"] == "stats":
+            await stats(websocket, decoded["stats"])
         elif decoded["rpc"] == "register":
-            register(websocket, decoded["data"])
+            await register(websocket, decoded["data"])
+            wslist[websocket._device_id] = websocket
         elif decoded["rpc"] == "output":
-            output(websocket, decoded["output"])
+            await output(websocket, decoded["output"])
+        elif decoded["rpc"] == "status":
+            await status(websocket, decoded["status"])
         else:
             pass
 
-asyncio.get_event_loop().run_until_complete(
-    websockets.serve(handle, '0.0.0.0', 8765))
-asyncio.get_event_loop().run_forever()
+try:
+    asyncio.get_event_loop().run_until_complete(
+        asyncio.gather(
+            websockets.serve(handle, '0.0.0.0', 5000), 
+            do_stuff_timer()
+        )
+    )
+    asyncio.get_event_loop().run_forever()
+except KeyboardInterrupt:
+    print("Exiting")
